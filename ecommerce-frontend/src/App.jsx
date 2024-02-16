da24d31ee98cb6327b027f0f67941984a781753c
Eder <julianeder97@gmail.com>
2024-02-16 16:38:28 +0100

    functionalComponentsWithBasicRoutes

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./components/home";
+import Login from "./components/login";
+import Register from "./components/register";
+import Profile from "./components/profile";
+
+import { AuthContext, AuthProvider } from "./context/context";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+export default App;
