b9957147d13ea0a32cc83cd7541dd98f1769b545
EderJulian <julianeder97@gmail.com>
2024-02-17 12:40:12 +0100

    register deleted dont need it

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./components/home";
 import Login from "./components/login";
-import Register from "./components/register";
 import Profile from "./components/profile";
 import TheHeader from "./components/theHeader";
 
