af306706dbfc7bf2beb9b55123d2f2ab0fec6834
Evgeny Goncharov <evgeny.goncharov.pro@gmail.com>
2024-02-23 15:42:18 +0100

    If logged /login and /register is not visible in the menu

--- ecommerce-frontend/src/assets/components/Theheader/theHeader.jsx
+++ ecommerce-frontend/src/assets/components/Theheader/theHeader.jsx
@@ -1,6 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { React, useState, useContext, useEffect } from "react";
-import { ProfileOutlined, AuditOutlined, LoginOutlined, AmazonOutlined, WindowsOutlined, DropboxOutlined } from "@ant-design/icons";
+import {
+  ProfileOutlined,
+  AuditOutlined,
+  LoginOutlined,
+  AmazonOutlined,
+  WindowsOutlined,
+  DropboxOutlined,
+} from "@ant-design/icons";
 import { Menu } from "antd";
 import "../../../App.scss";
 import { UserContext } from "../../../context/user/UserState";
@@ -10,16 +17,6 @@ const items = [
     label: "Home",
     key: "home",
     icon: <WindowsOutlined />,
-  },
-  {
-    label: "Login",
-    key: "login",
-    icon: <LoginOutlined />,
-  },
-  {
-    label: "Register",
-    key: "register",
-    icon: <AuditOutlined />,
   },
   {
     label: "Products",
@@ -38,10 +35,24 @@ const items = [
   },
 ];
 
+const authItems = [
+  {
+    label: "Login",
+    path: "/login",
+    icon: <LoginOutlined />,
+  },
+  {
+    label: "Register",
+    path: "/register",
+    icon: <AuditOutlined />,
+  },
+];
+
 const Header = () => {
   let navigate = useNavigate();
   const [current, setCurrent] = useState("home");
   const { token } = useContext(UserContext);
+  // const token = JSON.parse(localStorage.getItem("token"));
 
   useEffect(() => {
     if (!token) {
@@ -75,11 +86,28 @@ const Header = () => {
 
   return (
     <div>
-      {token && (
-        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items}>
+      {token ? (
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        >
           {items.map((item) => (
             <Menu.Item key={item.key} icon={item.icon}>
               {item.label}
+            </Menu.Item>
+          ))}
+        </Menu>
+      ) : (
+        <Menu
+          onClick={(e) => onClick(e.key)}
+          selectedKeys={[current]}
+          mode="horizontal"
+        >
+          {authItems.map((item) => (
+            <Menu.Item key={item.path} icon={item.icon}>
+              <Link to={item.path}>{item.label}</Link>
             </Menu.Item>
           ))}
         </Menu>
@@ -89,167 +117,3 @@ const Header = () => {
 };
 
 export default Header;
-
-
-// import { useNavigate } from "react-router-dom";
-// import { React, useState, useContext } from "react";
-// import { ProfileOutlined, AuditOutlined, LoginOutlined, AmazonOutlined, WindowsOutlined, DropboxOutlined } from "@ant-design/icons";
-// import { Menu } from "antd";
-// import "../../../App.scss";
-// import { UserContext } from "../../../context/user/UserState";
-
-// const items = [
-//   {
-//     label: "Home",
-//     key: "home",
-//     icon: <WindowsOutlined />,
-//   },
-//   {
-//     label: "Login",
-//     key: "login",
-//     icon: <LoginOutlined />,
-//   },
-//   {
-//     label: "Register",
-//     key: "register",
-//     icon: <AuditOutlined />,
-//   },
-//   {
-//     label: "Products",
-//     key: "products",
-//     icon: <AmazonOutlined />,
-//   },
-//   {
-//     label: "Cart",
-//     key: "cart",
-//     icon: <DropboxOutlined />,
-//   },
-//   {
-//     label: "Profile",
-//     key: "profile",
-//     icon: <ProfileOutlined />,
-//   },
-// ];
-
-// const Header = () => {
-//   let navigate = useNavigate();
-//   const [current, setCurrent] = useState("home");
-//   const { token } = useContext(UserContext);
-
-//   const onClick = (e) => {
-//     setCurrent(e.key);
-//     switch (e.key) {
-//       case "home":
-//         navigate("/");
-//         break;
-//       case "login":
-//         navigate("/login");
-//         break;
-//       case "register":
-//         navigate("/register");
-//         break;
-//       case "products":
-//         navigate("/products");
-//         break;
-//       case "profile":
-//         navigate("/profile");
-//         break;
-//       case "cart":
-//         navigate("/cart");
-//         break;
-//     }
-//   };
-
-//   return (
-//     <div>
-//       {token ? (
-//         <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items}>
-//           {items.map((item) => (
-//             <Menu.Item key={item.key} icon={item.icon}>
-//               {item.label}
-//             </Menu.Item>
-//           ))}
-//         </Menu>
-//       ) : (
-//         <div>
-//           <Link to="/login">Login</Link>
-//           <Link to="/register">Register</Link>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default Header;
-
-
-
-// const items = [
-//   {
-//     label: 'Home',
-//     key: 'home',
-//     icon: <WindowsOutlined />,
-//   },
-//   {
-//     label: 'Login',
-//     key: 'login',
-//     icon: <LoginOutlined />,
-    
-//   },
-//   {
-//     label: 'Register',
-//     key: 'register',
-//     icon: <AuditOutlined />,
-//   },
-//   {
-//     label: 'Products',
-//     key: 'products',
-//     icon: <AmazonOutlined />,
-//   },
-//   {
-//     label: 'Cart',
-//     key: 'cart',
-//     icon: <DropboxOutlined />,
-//   },
-//   {
-//     label: 'Profile',
-//     key: 'profile',
-//     icon: <ProfileOutlined />,
-//   },
-// ]
-
-// const Header = () => {
-//   let navigate = useNavigate()
-//   const [current, setCurrent] = useState('home')
-//   const {token} = useContext(AuthContext);
-
-//   const onClick = (e) => {
-//     setCurrent(e.key)
-//     switch (e.key){ 
-//           case 'home': 
-//             navigate('/')
-//             break
-//           case 'login': 
-//             navigate('/login')
-//             break
-//           case 'register': 
-//             navigate('/register')
-//             break
-//           case 'products': 
-//             navigate('/products')
-//             break
-//           case 'profile': 
-//             navigate('/profile')
-//             break
-// 		      case 'cart': 
-//             navigate('/cart')
-//             break
-		
-//     }
-//   };
-  
-
-//   return <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
-// }
-
-// export default Header
